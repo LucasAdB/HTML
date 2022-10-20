5531bdda83991d00b510be3786af041dce27b6bb
Lucas Andre Bento <lucas.bento1@alunos.sc.senac.br>
2022-10-19 21:49:14 -0300

    html

---
+++ Js/script.js
@@ -0,0 +1,16 @@
+// function mostraData(){
+	// document.getElementById("data".innerHTML=Date();
+	
+// }
+
+function MostrarDados(){
+var dados = "Lucas";
+alert(dados);
+var dados=23;
+alert(dados)
+var dados="true";
+alert(dados)
+var vetor=[1,"lucas",true];
+alert(vetor)
+
+}
